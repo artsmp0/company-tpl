41b7d33c6003963f7bcb178fb4790570266d45c3
沈孟平 <artsmp@126.com>
2023-09-14 14:18:20 +0800

    feat: useDark demo

---
+++ src/views/Demo/Hooks/UseDark/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { GpPageWrapper } from '@/components';
+import { isDark, toggleDark } from '@/composables';
+
+defineOptions({ name: 'Demo.Hooks.UseDark' });
+</script>
+
+<template>
+  <GpPageWrapper>
+    <NSpace align="center">
+      <NText>当前主题色：{{ isDark ? '暗色' : '亮色' }}</NText>
+      <NButton type="primary" @click="toggleDark()">切换明暗色</NButton>
+    </NSpace>
+  </GpPageWrapper>
+</template>

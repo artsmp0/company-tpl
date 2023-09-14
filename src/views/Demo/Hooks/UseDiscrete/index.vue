c138b44c26c631a83c545fbe2049ba88d84f0197
沈孟平 <artsmp@126.com>
2023-09-14 14:27:28 +0800

    feat: useDiscrete demo

---
+++ src/views/Demo/Hooks/UseDiscrete/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { GpPageWrapper } from '@/components';
+import { useDiscrete } from '@/composables';
+
+defineOptions({ name: 'Demo.Hooks.UseDiscrete' });
+const { dialog, message, notification } = useDiscrete();
+
+const showMsg = () => {
+  message.success('点击了『显示消息』按钮！');
+};
+
+const showNotify = () => {
+  notification.success({
+    content: '点击了『显示通知』按钮！',
+    title: '通知',
+    duration: 1000,
+  });
+};
+
+const showDialog = () => {
+  dialog.info({
+    title: '这是弹框',
+    content: '点击了『显示弹框』按钮！',
+  });
+};
+</script>
+
+<template>
+  <GpPageWrapper>
+    <NSpace>
+      <NButton @click="showMsg">显示消息</NButton>
+      <NButton @click="showNotify">显示通知</NButton>
+      <NButton @click="showDialog">显示弹框</NButton>
+    </NSpace>
+  </GpPageWrapper>
+</template>
